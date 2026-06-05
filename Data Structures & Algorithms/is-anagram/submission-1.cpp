5c324e265b31bc5f9b70ecf8ee68b6d8e6216f51
Tanishka Dhondge <74821382+Tanishqa-10@users.noreply.github.com>
2026-06-05 21:34:40 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        unordered_map <char, int> m1;
+        unordered_map <char, int> m2;
+
+        for(char c:s) m1[c]++;
+        for(char c:t) m2[c]++;
+
+        if(m1 == m2){
+            return true;
+        }
+
+        return false;
+
+    }
+};
